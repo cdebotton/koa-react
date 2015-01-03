854a8ebb335f4ab5d31a3beeb62edfa30f305235
Christian de Botton <cdebotton@gmail.com>
2015-01-03 14:26:00 -0500

    Adapter store now partially wired up.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ require('6to5/register');
 
 exports.KoaReact = require('./lib/KoaReact');
 exports.Config = require('./lib/Config');
+exports.fetchData = require('./lib/utils/middleware/helpers/fetchData');
